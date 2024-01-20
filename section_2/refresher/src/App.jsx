09aebf705b41b0ea938574ced380818a288dc4ef
yonghowfung <yhfung940217@gmail.com>
2024-01-20 20:54:30 +0100

    Revert "modified"
    This reverts commit 5b9e9de8b4dbbf522d7e5c2e245a5854cd840d6e.

--- section_2/refresher/src/App.jsx
+++ section_2/refresher/src/App.jsx
@@ -1,19 +1,11 @@
-import GoalList from "./components/GoalList";
 import "./App.css";
 
-const App = () => {
-  const courseGoals = [
-    { id: "cg1", text: "Finish the Course" },
-    { id: "cg2", text: "Learn all about the Course Main Topic" },
-    { id: "cg3", text: "Help other students in the Course Q&A" },
-  ];
-
+function App() {
   return (
     <>
-      <h1>Course Goals</h1>
-      <GoalList goals={courseGoals} />
+      <h1>A React App!</h1>
     </>
   );
-};
+}
 
 export default App;
