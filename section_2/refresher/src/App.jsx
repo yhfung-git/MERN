5b9e9de8b4dbbf522d7e5c2e245a5854cd840d6e
yonghowfung <yhfung940217@gmail.com>
2024-01-20 20:53:15 +0100

    modified import ./App

--- section_2/refresher/src/App.jsx
+++ section_2/refresher/src/App.jsx
@@ -1,11 +1,19 @@
+import GoalList from "./components/GoalList";
 import "./App.css";
 
-function App() {
+const App = () => {
+  const courseGoals = [
+    { id: "cg1", text: "Finish the Course" },
+    { id: "cg2", text: "Learn all about the Course Main Topic" },
+    { id: "cg3", text: "Help other students in the Course Q&A" },
+  ];
+
   return (
     <>
-      <h1>A React App!</h1>
+      <h1>Course Goals</h1>
+      <GoalList goals={courseGoals} />
     </>
   );
-}
+};
 
 export default App;
