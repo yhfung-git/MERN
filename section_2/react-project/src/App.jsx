de67212f32c13458e6d58772fa0da41f00de5667
yonghowfung <yhfung940217@gmail.com>
2024-01-21 22:09:26 +0100

    added new path for User Places

--- section_2/react-project/src/App.jsx
+++ section_2/react-project/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import MainNavigation from "./shared/components/Navigation/MainNavigation/MainNavigation";
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import UserPlaces from "./places/pages/UserPlaces";
 import "./App.css";
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
       <main>
         <Routes>
           <Route path="/" element={<Users />} />
+          <Route path="/:userId/places" element={<UserPlaces />} />
           <Route path="/places/new" element={<NewPlace />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
