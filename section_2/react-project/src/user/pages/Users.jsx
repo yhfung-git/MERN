fdb059fc7aec2d9ec026dda8d1bfa1cf8fb86ee5
yonghowfung <yhfung940217@gmail.com>
2024-01-21 22:12:39 +0100

    removed title

--- section_2/react-project/src/user/pages/Users.jsx
+++ section_2/react-project/src/user/pages/Users.jsx
@@ -2,7 +2,7 @@ import UsersList from "../components/UsersList/UsersList";
 
 const USERS = [
   {
-    id: Date.now().toString(),
+    id: "u1",
     name: "Howard",
     image: "https://media.timeout.com/images/105481350/750/562/image.jpg",
     places: 3,
@@ -10,12 +10,7 @@ const USERS = [
 ];
 
 const Users = () => {
-  return (
-    <>
-      <h1 className="pageTitle">Users List</h1>
-      <UsersList users={USERS} />
-    </>
-  );
+  return <UsersList users={USERS} />;
 };
 
 export default Users;
