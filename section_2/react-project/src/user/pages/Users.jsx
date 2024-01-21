4c2e2a4318b9fa16e187fa9d2243086a04adbe30
yonghowfung <yhfung940217@gmail.com>
2024-01-21 21:47:43 +0100

    moved dummy data outside Users function component

--- section_2/react-project/src/user/pages/Users.jsx
+++ section_2/react-project/src/user/pages/Users.jsx
@@ -1,15 +1,15 @@
 import UsersList from "../components/UsersList/UsersList";
 
-const Users = () => {
-  const USERS = [
-    {
-      id: Date.now().toString(),
-      name: "Howard",
-      image: "https://media.timeout.com/images/105481350/750/562/image.jpg",
-      places: 3,
-    },
-  ];
+const USERS = [
+  {
+    id: Date.now().toString(),
+    name: "Howard",
+    image: "https://media.timeout.com/images/105481350/750/562/image.jpg",
+    places: 3,
+  },
+];
 
+const Users = () => {
   return (
     <>
       <h1 className="pageTitle">Users List</h1>
