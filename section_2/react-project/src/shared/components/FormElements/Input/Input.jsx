1085ecfbbb33f9537f52fc8cde280b3461e83960
yonghowfung <yhfung940217@gmail.com>
2024-01-22 22:54:14 +0100

    create a input component for form

---
+++ section_2/react-project/src/shared/components/FormElements/Input/Input.jsx
@@ -0,0 +1,66 @@
+import { useReducer } from "react";
+
+import { validate } from "../../../util/validators";
+import "./Input.css";
+
+const inputReducer = (state, action) => {
+  switch (action.type) {
+    case "CHANGE":
+      return {
+        ...state,
+        value: action.value,
+        isValid: validate(action.value, action.validators),
+      };
+    default:
+      return state;
+  }
+};
+
+const Input = (props) => {
+  const [inputState, dispatch] = useReducer(inputReducer, {
+    value: "",
+    isValid: true,
+  });
+
+  const changeHandler = (event) => {
+    dispatch({
+      type: "CHANGE",
+      value: event.target.value,
+      validators: props.validators,
+    });
+  };
+
+  const element =
+    props.element === "input" ? (
+      <input
+        id={props.id}
+        type={props.type}
+        placeholder={props.placeholder}
+        onChange={changeHandler}
+        onBlur={changeHandler}
+        value={inputState.value}
+      />
+    ) : (
+      <textarea
+        id={props.id}
+        rows={props.rows || 3}
+        onChange={changeHandler}
+        onBlur={changeHandler}
+        value={inputState.value}
+      />
+    );
+
+  return (
+    <div
+      className={`form-control ${
+        !inputState.isValid && "form-control--invalid"
+      }`}
+    >
+      <label htmlFor={props.id}>{props.label}</label>
+      {element}
+      {!inputState.isValid && <p>{props.errorText}</p>}
+    </div>
+  );
+};
+
+export default Input;
