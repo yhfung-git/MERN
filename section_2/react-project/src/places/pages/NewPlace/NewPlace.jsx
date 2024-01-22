1d03dd3b0e9c2d92fdaa16cce0c10c09f22bf2c0
yonghowfung <yhfung940217@gmail.com>
2024-01-22 22:54:42 +0100

    use Input to the form

--- section_2/react-project/src/places/pages/NewPlace/NewPlace.jsx
+++ section_2/react-project/src/places/pages/NewPlace/NewPlace.jsx
@@ -1,5 +1,21 @@
+import { VALIDATOR_REQUIRE } from "../../../shared/util/validators";
+import Input from "../../../shared/components/FormElements/Input/Input";
+import "./NewPlace.css";
+
 const NewPlace = () => {
-  return <h2>NewPlace Works!</h2>;
+  return (
+    <form className="place-form">
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        placeholder="Enter a title..."
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title"
+      />
+    </form>
+  );
 };
 
 export default NewPlace;
