2f30d2bb106a5c79af9ff8a3e7a0ed6fd1fcff65
yonghowfung <yhfung940217@gmail.com>
2024-01-23 17:44:37 +0100

    for storing dummy data

---
+++ section_2/react-project/places.jsx
@@ -0,0 +1,21 @@
+export const PLACES = [
+  {
+    id: "p1",
+    title: "Les Deux Alpes",
+    description: "Les Deux Alpes is a ski resort in France.",
+    image: "https://upload.wikimedia.org/wikipedia/commons/8/8c/Les2Alpes.jpg",
+    address: "ZAC le, Clos des Fonds, 38860 Les Deux Alpes",
+    location: { lat: 45.0055207, lng: 6.0958591 },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Exploratorium",
+    description: "A museum of science, technology, and arts in San Francisco.",
+    image:
+      "https://media.tacdn.com/media/attractions-splice-spp-674x446/0b/27/54/8c.jpg",
+    address: "Pier 15 Embarcadero at, Green St, San Francisco, CA 94111",
+    location: { lat: 37.796262, lng: -122.4043911 },
+    creator: "u2",
+  },
+];
