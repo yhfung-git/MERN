04ad61c483a5b514bf4a33f9122c7e86d0ab6d04
yonghowfung <yhfung940217@gmail.com>
2024-01-24 21:30:49 +0100

    seperate routes for authenticated users

--- section_2/react-project/src/App.jsx
+++ section_2/react-project/src/App.jsx
@@ -1,5 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import { useContext } from "react";
 
+import { AuthContext } from "./shared/context/auth-context";
 import MainNavigation from "./shared/components/Navigation/MainNavigation/MainNavigation";
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace/NewPlace";
@@ -9,19 +11,28 @@ import Auth from "./user/pages/Auth/Auth";
 import "./App.css";
 
 const App = () => {
+  const auth = useContext(AuthContext);
+  const routes = auth.isLoggedIn ? (
+    <Routes>
+      <Route path="/" element={<Users />} />
+      <Route path="/places/new" element={<NewPlace />} />
+      <Route path="/places/:placeId" element={<UpdatePlace />} />
+      <Route path="/:userId/places" element={<UserPlaces />} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  ) : (
+    <Routes>
+      <Route path="/" element={<Users />} />
+      <Route path="/auth" element={<Auth />} />
+      <Route path="/:userId/places" element={<UserPlaces />} />
+      <Route path="*" element={<Navigate to="/auth" />} />
+    </Routes>
+  );
+
   return (
     <>
       <MainNavigation />
-      <main>
-        <Routes>
-          <Route path="/" element={<Users />} />
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/places/new" element={<NewPlace />} />
-          <Route path="/places/:placeId" element={<UpdatePlace />} />
-          <Route path="/:userId/places" element={<UserPlaces />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </main>
+      <main>{routes}</main>
     </>
   );
 };
