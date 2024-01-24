d12cb93a34fb610bca3780bd0c4cd2489426abcf
yonghowfung <yhfung940217@gmail.com>
2024-01-24 19:42:22 +0100

    wrap auth context for entire app

--- section_2/react-project/src/main.jsx
+++ section_2/react-project/src/main.jsx
@@ -1,11 +1,14 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
+import AuthContextProvider from "./shared/context/AuthContextProvider";
 import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <AuthContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AuthContextProvider>
 );
