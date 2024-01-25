be9ba546bf8010b3037a5fc8f5f2adb5186e2616
yonghowfung <yhfung940217@gmail.com>
2024-01-25 21:30:56 +0100

    app.js for routes, server, global error handling

---
+++ section_3/backend/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const placesRoutes = require("./routes/placesRoutes");
+const usersRoutes = require("./routes/usersRoutes");
+
+const app = express();
+const port = 5000;
+
+app.use(express.json());
+
+app.use("/api/users", usersRoutes);
+app.use("/api/places", placesRoutes);
+
+app.use((error, req, res, next) => {
+  if (res.headersSent) {
+    console.error(">>> error-handling middleware", error);
+    return next(error);
+  }
+
+  const statusCode = error.statusCode || 500;
+  const errorMessage = error.message || "An unknown error occurred!";
+  res.status(statusCode).json({ message: errorMessage });
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
