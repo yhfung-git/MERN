7a12ecba131df7881e02d46b881e3501b9ba0b64
yonghowfung <yhfung940217@gmail.com>
2024-01-27 18:45:33 +0100

    connect to products controllers

--- section_4/refresher/app.js
+++ section_4/refresher/app.js
@@ -1,13 +1,15 @@
 const express = require("express");
 
+const { createProduct, getProduct } = require("./productsControllers");
+
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-app.post("/products");
+app.post("/products", createProduct);
 
-app.get("/products");
+app.get("/products", getProduct);
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
