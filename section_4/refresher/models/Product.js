46c31e25659189ae474ca31bcb5ba8729dfc04ee
yonghowfung <yhfung940217@gmail.com>
2024-01-27 21:14:09 +0100

    product model for mongoose

---
+++ section_4/refresher/models/Product.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const productSchema = new Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+});
+
+module.exports = mongoose.model("Product", productSchema);
