3196cd00023861b1be3d8dede6dd9424fdcdabe1
yonghowfung <yhfung940217@gmail.com>
2024-01-28 19:09:56 +0100

    added cors

--- section_4/backend/app.js
+++ section_4/backend/app.js
@@ -13,6 +13,16 @@ const port = 5000;
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Accesc-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
+
 app.use("/api/users", usersRoutes);
 app.use("/api/places", placesRoutes);
 app.use(errorsRoutes);
