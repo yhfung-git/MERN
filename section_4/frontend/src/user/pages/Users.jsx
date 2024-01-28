5bd1c60c617cccb4b047dff062a7c92592ac5128
yonghowfung <yhfung940217@gmail.com>
2024-01-29 00:00:43 +0100

    fetch users list

--- section_4/frontend/src/user/pages/Users.jsx
+++ section_4/frontend/src/user/pages/Users.jsx
@@ -1,9 +1,46 @@
-import UsersList from "../components/UsersList/UsersList";
+import { useState, useEffect } from "react";
 
-import { USERS } from "../../DUMMY_DATA";
+import UsersList from "../components/UsersList/UsersList";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner/LoadingSpinner";
 
 const Users = () => {
-  return <UsersList users={USERS} />;
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch("http://localhost:5000/api/users");
+        const data = await response.json();
+        if (!response.ok) throw new Error(data.message);
+
+        setUsers(data.users);
+      } catch (error) {
+        setError(error.message || "Something went wrong, please try again.");
+      }
+      setIsLoading(false);
+    };
+    getUsers();
+  }, []);
+
+  const closeErrorModalHandler = () => {
+    setError(null);
+  };
+
+  return (
+    <>
+      <ErrorModal error={error} onClear={closeErrorModalHandler} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      <UsersList users={users} />
+    </>
+  );
 };
 
 export default Users;
