72cda161d0fd406392205270f368b1261e927c25
yonghowfung <yhfung940217@gmail.com>
2024-01-28 02:43:51 +0100

    added timestamp

--- section_4/backend/models/Place.js
+++ section_4/backend/models/Place.js
@@ -1,37 +1,41 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
-const placeSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-    required: true,
-  },
-  address: {
-    type: String,
-    required: true,
-  },
-  location: {
-    lat: {
-      type: Number,
+const placeSchema = new Schema(
+  {
+    title: {
+      type: String,
       required: true,
     },
-    lng: {
-      type: Number,
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    location: {
+      lat: {
+        type: Number,
+        required: true,
+      },
+      lng: {
+        type: Number,
+        required: true,
+      },
+    },
+    creator: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
       required: true,
     },
   },
-  creator: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = model("Place", placeSchema);
