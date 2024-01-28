6d2d5738f9e32b5c478a93022b4ec5c53d64c86e
yonghowfung <yhfung940217@gmail.com>
2024-01-28 18:55:37 +0100

    modified singup to signup

--- section_4/backend/routes/usersRoutes.js
+++ section_4/backend/routes/usersRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 
-const { getUsers, singup, login } = require("../controllers/usersControllers");
+const { getUsers, signup, login } = require("../controllers/usersControllers");
 const { checkSignupInput } = require("../helpers/validators");
 
 const router = express.Router();
 
-router.post("/signup", checkSignupInput, singup);
+router.post("/signup", checkSignupInput, signup);
 
 router.post("/login", login);
 
