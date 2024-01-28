22209de046467058851e1064a62db3552b8207d4
yonghowfung <yhfung940217@gmail.com>
2024-01-28 23:59:40 +0100

    changed id to _id

--- section_4/frontend/src/user/components/UserItem/UserItem.jsx
+++ section_4/frontend/src/user/components/UserItem/UserItem.jsx
@@ -8,14 +8,14 @@ const UserItem = (props) => {
   return (
     <li className="user-item">
       <Card className="user-item__content">
-        <Link to={`/${props.id}/places`}>
+        <Link to={`/${props._id}/places`}>
           <div className="user-item__image">
             <Avatar image={props.image} alt={props.name} />
           </div>
           <div className="user-item__info">
             <h2>{props.name}</h2>
             <h3>
-              {props.places} {props.places === 1 ? "place" : "places"}
+              {props.places.length} {props.places <= 1 ? "place" : "places"}
             </h3>
           </div>
         </Link>
