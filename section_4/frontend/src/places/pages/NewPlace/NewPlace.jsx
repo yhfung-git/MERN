3436104e7dfa7bf6359997b14599bdca64f9b89f
yonghowfung <yhfung940217@gmail.com>
2024-01-29 15:25:04 +0100

    use custom fetch api hook

--- section_4/frontend/src/places/pages/NewPlace/NewPlace.jsx
+++ section_4/frontend/src/places/pages/NewPlace/NewPlace.jsx
@@ -1,4 +1,9 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { AuthContext } from "../../../shared/context/auth-context";
 import { useForm } from "../../../shared/hooks/form-hook";
+import { useHttpClient } from "../../../shared/hooks/http-hook";
 import {
   VALIDATOR_MINLENGTH,
   VALIDATOR_REQUIRE,
@@ -6,8 +11,13 @@ import {
 import Button from "../../../shared/components/FormElements/Button/Button";
 import Input from "../../../shared/components/FormElements/Input/Input";
 import "./NewPlace.css";
+import LoadingSpinner from "../../../shared/components/UIElements/LoadingSpinner/LoadingSpinner";
+import ErrorModal from "../../../shared/components/UIElements/ErrorModal/ErrorModal";
 
 const NewPlace = () => {
+  const auth = useContext(AuthContext);
+  const navigate = useNavigate();
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
   const [formState, inputHandler] = useForm(
     {
       title: { value: "", isValid: false },
@@ -17,47 +27,63 @@ const NewPlace = () => {
     false
   );
 
-  const placeSubmitHandler = (event) => {
+  const placeSubmitHandler = async (event) => {
     event.preventDefault();
 
-    console.log("Form Submitted:", formState.inputs);
+    const response = await sendRequest(
+      "http://localhost:5000/api/places/new",
+      "POST",
+      JSON.stringify({
+        title: formState.inputs.title.value,
+        description: formState.inputs.description.value,
+        address: formState.inputs.address.value,
+        creator: auth.userId,
+      }),
+      { "Content-Type": "application/json" }
+    );
+
+    if (response !== null) navigate(`/${auth.userId}/places`);
   };
 
   return (
-    <form className="place-form" onSubmit={placeSubmitHandler}>
-      <Input
-        id="title"
-        element="input"
-        type="text"
-        label="Title"
-        placeholder="Enter a title..."
-        validators={[VALIDATOR_REQUIRE()]}
-        errorText="Please enter a valid title"
-        onInput={inputHandler}
-      />
-      <Input
-        id="address"
-        element="input"
-        type="text"
-        label="Address"
-        placeholder="Enter an address..."
-        validators={[VALIDATOR_REQUIRE()]}
-        errorText="Please enter a valid address"
-        onInput={inputHandler}
-      />
-      <Input
-        id="description"
-        element="textarea"
-        label="Description"
-        placeholder="Enter a description..."
-        validators={[VALIDATOR_MINLENGTH(5)]}
-        errorText="Please enter at least 5 characters"
-        onInput={inputHandler}
-      />
-      <Button type="submit" disabled={!formState.isValid}>
-        ADD PLACE
-      </Button>
-    </form>
+    <>
+      <ErrorModal error={error} onClear={clearError} />
+      <form className="place-form" onSubmit={placeSubmitHandler}>
+        {isLoading && <LoadingSpinner asOverlay />}
+        <Input
+          id="title"
+          element="input"
+          type="text"
+          label="Title"
+          placeholder="Enter a title..."
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText="Please enter a valid title"
+          onInput={inputHandler}
+        />
+        <Input
+          id="address"
+          element="input"
+          type="text"
+          label="Address"
+          placeholder="Enter an address..."
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText="Please enter a valid address"
+          onInput={inputHandler}
+        />
+        <Input
+          id="description"
+          element="textarea"
+          label="Description"
+          placeholder="Enter a description..."
+          validators={[VALIDATOR_MINLENGTH(5)]}
+          errorText="Please enter at least 5 characters"
+          onInput={inputHandler}
+        />
+        <Button type="submit" disabled={!formState.isValid}>
+          ADD PLACE
+        </Button>
+      </form>
+    </>
   );
 };
 
