723db13ebc75365926d4ccee17a8e3e1063731f0
yonghowfung <yhfung940217@gmail.com>
2024-01-29 17:45:24 +0100

    coorection typo

--- section_4/backend/app.js
+++ section_4/backend/app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Accesc-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
