a0f494ebe34ada8d0e4ae3967286558f597affbd
yonghowfung <yhfung940217@gmail.com>
2024-01-29 19:40:38 +0100

    pass either userId or user._id based on loin or singup

--- section_4/frontend/src/user/pages/Auth/Auth.jsx
+++ section_4/frontend/src/user/pages/Auth/Auth.jsx
@@ -71,7 +71,7 @@ const Auth = () => {
           { "Content-Type": "application/json" }
         );
 
-    if (response !== null) auth.login(response.userId);
+    if (response !== null) auth.login(response.userId || response.user._id);
   };
 
   return (
