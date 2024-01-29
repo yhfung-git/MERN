5b12bb75b239e38cfc1c68488d6b8b8adf8a62c1
yonghowfung <yhfung940217@gmail.com>
2024-01-29 15:35:04 +0100

    use custom hook to fetch apis

--- section_4/frontend/src/places/pages/UserPlaces/UserPlaces.jsx
+++ section_4/frontend/src/places/pages/UserPlaces/UserPlaces.jsx
@@ -1,12 +1,39 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
+import { useHttpClient } from "../../../shared/hooks/http-hook";
 import PlaceList from "../../components/PlaceList/PlaceList";
-import { PLACES } from "../../../DUMMY_DATA";
+import LoadingSpinner from "../../../shared/components/UIElements/LoadingSpinner/LoadingSpinner";
+import ErrorModal from "../../../shared/components/UIElements/ErrorModal/ErrorModal";
 
 const UserPlaces = () => {
   const { userId } = useParams();
-  const loadedPlaces = PLACES.filter((place) => place.creator === userId);
-  return <PlaceList places={loadedPlaces} />;
+  const [places, setPlaces] = useState([]);
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+
+  useEffect(() => {
+    const fetchUserPlaces = async () => {
+      const response = await sendRequest(
+        `http://localhost:5000/api/places/user/${userId}`
+      );
+
+      if (response !== null) setPlaces(response.places);
+    };
+
+    fetchUserPlaces();
+  }, [sendRequest, userId]);
+
+  return (
+    <>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      <PlaceList places={places} />
+    </>
+  );
 };
 
 export default UserPlaces;
