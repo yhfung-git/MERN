f7c74e19585403d29b3a69ac72f4ec85a7d5ab7a
yonghowfung <yhfung940217@gmail.com>
2024-01-30 21:59:58 +0100

    created a middleware to verify token

---
+++ section_4/backend/middlewares/isAuth.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+const { JWT_PASSKEY } = process.env;
+const { throwError } = require("../helpers/errorHandler");
+
+module.exports = async (req, res, next) => {
+  try {
+    const token = await req.get("Authorization")?.split(" ")[1];
+    if (!token) throwError(401, "Invalid token");
+
+    const decoded = jwt.verify(token, JWT_PASSKEY, { ignoreExpiration: false });
+    if (!decoded) throwError(401, "Not authenticated");
+
+    const currentTimestamp = Math.floor(Date.now() / 1000);
+    if (decoded.exp && decoded.exp < currentTimestamp) {
+      throwError(401, "Token has expired");
+    }
+
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    console.error(">>> auth", error);
+    next(error);
+  }
+};
