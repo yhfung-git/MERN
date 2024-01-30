016ef4220e32b36bdf3e7a24095f7772f0a06e68
yonghowfung <yhfung940217@gmail.com>
2024-01-30 16:48:03 +0100

    added fileUpload function to handle image

--- section_4/backend/routes/usersRoutes.js
+++ section_4/backend/routes/usersRoutes.js
@@ -2,10 +2,11 @@ const express = require("express");
 
 const { getUsers, signup, login } = require("../controllers/usersControllers");
 const { checkSignupInput } = require("../helpers/validators");
+const fileUpload = require("../middlewares/fileUpload");
 
 const router = express.Router();
 
-router.post("/signup", checkSignupInput, signup);
+router.post("/signup", fileUpload.single("image"), checkSignupInput, signup);
 
 router.post("/login", login);
 
