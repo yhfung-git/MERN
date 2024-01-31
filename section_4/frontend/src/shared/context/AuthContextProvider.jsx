6b7e3561103a3e7b10cf2282102c0e485da6f709
yonghowfung <yhfung940217@gmail.com>
2024-01-31 20:15:16 +0100

    enhanced login logout and keep logging after refreshing pages

--- section_4/frontend/src/shared/context/AuthContextProvider.jsx
+++ section_4/frontend/src/shared/context/AuthContextProvider.jsx
@@ -1,26 +1,46 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "./auth-context";
+import { useHttpClient } from "../hooks/http-hook";
 
 const AuthContextProvider = (props) => {
-  const [isLoggedIn, setIsLoggedIn] = useState();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userId, setUserId] = useState();
+  const { sendRequest } = useHttpClient();
   const navigate = useNavigate();
 
-  const login = useCallback(
-    (uid) => {
-      setIsLoggedIn(uid);
-      setUserId(uid);
-      navigate("/");
-    },
-    [navigate]
-  );
+  const login = useCallback((uid) => {
+    setIsLoggedIn(true);
+    setUserId(uid);
+    localStorage.setItem("loggedIn", "true");
+  }, []);
 
-  const logout = useCallback(() => {
-    setIsLoggedIn(null);
-    setUserId(null);
-    navigate("/auth");
-  }, [navigate]);
+  const logout = useCallback(async () => {
+    const response = await sendRequest(
+      "http://localhost:5000/api/users/logout"
+    );
+    if (response !== null) {
+      setIsLoggedIn(false);
+      setUserId(null);
+      localStorage.removeItem("loggedIn");
+      navigate("/auth");
+    }
+  }, [navigate, sendRequest]);
+
+  useEffect(() => {
+    const checkAuthStatus = async () => {
+      const loggedIn = localStorage.getItem("loggedIn");
+
+      if (loggedIn === "true") {
+        const response = await sendRequest(
+          "http://localhost:5000/api/users/auth-status"
+        );
+
+        if (response !== null) login(response.userId);
+      }
+    };
+    checkAuthStatus();
+  }, [sendRequest, login]);
 
   const authContextValue = {
     isLoggedIn,
