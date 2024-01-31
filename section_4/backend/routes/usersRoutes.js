a41ff78593fe581a0497f60c891b963f234dfa5f
yonghowfung <yhfung940217@gmail.com>
2024-01-31 20:13:42 +0100

    added auth-status and logout route

--- section_4/backend/routes/usersRoutes.js
+++ section_4/backend/routes/usersRoutes.js
@@ -1,10 +1,21 @@
 const express = require("express");
 
-const { getUsers, signup, login } = require("../controllers/usersControllers");
+const {
+  getUsers,
+  signup,
+  login,
+  checkAuthStatus,
+  logout,
+} = require("../controllers/usersControllers");
 const { checkSignupInput } = require("../helpers/validators");
 const fileUpload = require("../middlewares/fileUpload");
+const isAuth = require("../middlewares/isAuth");
 
 const router = express.Router();
+
+router.get("/auth-status", isAuth, checkAuthStatus);
+
+router.get("/logout", isAuth, logout);
 
 router.post("/signup", fileUpload.single("image"), checkSignupInput, signup);
 
