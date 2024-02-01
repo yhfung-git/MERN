2868d7fa24eb8df444bec04b249ca03d9cda9baf
yonghowfung <yhfung940217@gmail.com>
2024-02-01 14:40:30 +0100

    use lazy and Suspense on routes

--- section_4/frontend/src/App.jsx
+++ section_4/frontend/src/App.jsx
@@ -1,14 +1,18 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, lazy, Suspense } from "react";
 
 import { AuthContext } from "./shared/context/auth-context";
 import MainNavigation from "./shared/components/Navigation/MainNavigation/MainNavigation";
-import Users from "./user/pages/Users";
-import NewPlace from "./places/pages/NewPlace/NewPlace";
-import UserPlaces from "./places/pages/UserPlaces/UserPlaces";
-import UpdatePlace from "./places/pages/UpdatePlace/UpdatePlace";
-import Auth from "./user/pages/Auth/Auth";
+import LoadingSpinner from "./shared/components/UIElements/LoadingSpinner/LoadingSpinner";
 import "./App.css";
+
+const Users = lazy(() => import("./user/pages/Users"));
+const NewPlace = lazy(() => import("./places/pages/NewPlace/NewPlace"));
+const UserPlaces = lazy(() => import("./places/pages/UserPlaces/UserPlaces"));
+const UpdatePlace = lazy(() =>
+  import("./places/pages/UpdatePlace/UpdatePlace")
+);
+const Auth = lazy(() => import("./user/pages/Auth/Auth"));
 
 const App = () => {
   const auth = useContext(AuthContext);
@@ -32,7 +36,17 @@ const App = () => {
   return (
     <>
       <MainNavigation />
-      <main>{routes}</main>
+      <main>
+        <Suspense
+          fallback={
+            <div className="center">
+              <LoadingSpinner />
+            </div>
+          }
+        >
+          {routes}
+        </Suspense>
+      </main>
     </>
   );
 };
