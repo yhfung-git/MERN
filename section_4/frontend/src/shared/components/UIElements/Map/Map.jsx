e0c72cde5ec8ddb449d2e21208c3520826243f83
yonghowfung <yhfung940217@gmail.com>
2024-02-02 22:34:32 +0100

    added custom icon

--- section_4/frontend/src/shared/components/UIElements/Map/Map.jsx
+++ section_4/frontend/src/shared/components/UIElements/Map/Map.jsx
@@ -1,11 +1,20 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
 
 import "leaflet/dist/leaflet.css";
 import "./Map.css";
+import Icon from "../../../../../public/icon.svg";
 
 const Map = (props) => {
   const { lat, lng } = props.location;
   const position = [lat, lng];
+
+  const customIcon = new L.Icon({
+    iconUrl: Icon,
+    iconSize: [40, 40],
+    iconAnchor: [20, 40],
+    popupAnchor: [0, -40],
+  });
 
   return (
     <MapContainer
@@ -18,7 +27,7 @@ const Map = (props) => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={position}>
+      <Marker position={position} icon={customIcon}>
         <Popup>
           <div>
             <h3>{props.address}</h3>
