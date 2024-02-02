fa7b3515848cf63a71fa5e04b7d4efb45dd8a2f9
yonghowfung <yhfung940217@gmail.com>
2024-02-02 16:43:02 +0100

    middleware for image upload and delete via cloudinary

--- section_4/backend/middlewares/fileUpload.js
+++ section_4/backend/middlewares/fileUpload.js
@@ -1,16 +1,17 @@
 const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
 
-const fileUpload = multer({
-  // limits at 6MB
-  limits: 6 * 1024 * 1024,
-  storage: multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, "uploads/images");
-    },
-    filename: (req, file, cb) => {
-      cb(null, `${Date.now()}-${file.originalname}`);
-    },
-  }),
+const { CLOUD_NAME, CLOUD_KEY, CLOUD_SECRET } = process.env;
+
+cloudinary.config({
+  cloud_name: CLOUD_NAME,
+  api_key: CLOUD_KEY,
+  api_secret: CLOUD_SECRET,
+  secure: true,
+});
+
+exports.handleUpload = multer({
+  limits: { fileSize: 10 * 1024 * 1024 },
   fileFilter: (req, file, cb) => {
     const acceptedImageTypes = ["image/png", "image/jpg", "image/jpeg"];
     const isValid = acceptedImageTypes.includes(file.mimetype);
@@ -18,6 +19,38 @@ const fileUpload = multer({
 
     cb(error, isValid);
   },
+  storage: multer.memoryStorage(),
 });
 
-module.exports = fileUpload;
+exports.uploadImage = async (image) => {
+  try {
+    return new Promise((resolve, reject) => {
+      const stream = cloudinary.uploader.upload_stream(
+        { resource_type: "image" },
+        (error, result) => {
+          error ? reject(error) : resolve(result.secure_url);
+        }
+      );
+
+      stream.end(image.buffer);
+    });
+  } catch (error) {
+    console.error(">>> uploadImage", error);
+    throw error;
+  }
+};
+
+exports.extractImageId = async (image) => {
+  return new Promise((resolve, reject) => {
+    try {
+      const parts = image.split("/");
+      const fileName = parts[parts.length - 1];
+      const imageId = fileName.split(".")[0];
+
+      resolve(imageId);
+    } catch (error) {
+      console.error(">>> extractImageId", error);
+      reject(error);
+    }
+  });
+};
